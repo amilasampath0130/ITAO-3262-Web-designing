b7a4ee074db1ea0cb17498d0fc12dcb86e6c450c
R M A Sampath <amilasampath0130@gmail.com>
2026-05-16 10:14:04 +0530

    FEAT: fix the app layout first

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Image from 'next/image'
-import React from 'react'
-
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Image src="" alt="Description of image" width={500} height={300} />
+      <h1 className="text-3xl font-semibold">Welcome to the store</h1>
+      <p className="mt-4 text-gray-600">
+        Start building your homepage content here.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
